f6dc7d75fc2fa7d28f121f676cbcda03a108ffc3
Abhishek21-14 <rohitsaini2790@gmail.com>
2026-02-01 12:06:02 +0000

    two houre done

--- basic01/test.js
+++ basic01/test.js
@@ -1,1 +1,112 @@
-console.log("Rohit");
+// console.log("Rohit");
+// const account_id = 12345
+// let accountEmail = "rohitsaini27@gmail.gom"
+// var accountPassword = "12345"
+// let accountState;
+
+// // account_id = 2
+// console.log(accountEmail);
+// accountEmail = "rohit@google.com"
+// accountPassword = "2345"
+// console.table([accountEmail, accountPassword, account_id, accountState])
+
+// console.log(typeof null)  // output this is object 
+// console.log(typeof undefined);  // this is undefined
+
+// let score = "null"
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// let IsLoggedIn = "rohti"
+
+// let booleanIsNumber = Boolean(IsLoggedIn)
+// console.log(booleanIsNumber);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Rohit" => True
+
+// let string = 35
+
+// let isstring = String(string)
+// console.log(isstring)
+// console.log(typeof isstring)
+
+// ******************Operation************
+// let value = 5
+// let nagetive = (-value)
+// console.log(nagetive)
+
+
+// console.log(2+"1")
+// console.log("2"+1)
+// console.log(2+"1"+2)
+// console.log(2+1+"2")
+
+// console.log(2 > 1)
+// console.log("2ss" > 1)
+// console.log(2 > "3aa")
+// console.log(2 > "01")
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// const bigNumber = 2124648545354548n
+// console.log(typeof bigNumber);
+
+
+// const score = 33
+// const scoreValue = 100.3
+
+// const loggedIn = false
+// const temp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// const heros = ["Shaktiman", "naagraj", "spiderman"]
+
+// const myObject = {
+//     name: "rohit",
+//     age: 24
+// }
+
+// const myFunction = function(){
+//     console.log("Hello Rohit")    
+// }
+
+// console.log(typeof id)
+// myFunction;
+
+// *********************************************
+//  Stack (Primitive)  and Heap (Non-Primitive Value)
+
+// let myName = "Rohit"
+
+// let meName = myName;
+// meName = "Abhishek"
+
+// console.log(myName);
+// console.log(meName);
+
+// let FirstName = {
+//     Name: "Rohit",
+//     Age: 25
+// }
+
+// let SecName = FirstName;
+// SecName.Name = "AbhiSHek";
+
+// console.log(FirstName.Name)
+// console.log(SecName.Name)
+
+
